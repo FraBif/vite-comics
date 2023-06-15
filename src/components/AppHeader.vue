b6d21de0fe126919853372e77076c7a538fb6a58
FraBif <francescobif@hotmail.it>
2023-06-15 19:28:21 +0200

    tryin to v-for in the header navbar but it doesnt work

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,43 @@
 <template>
-    
+    <header>
+        <div class="logo-container">
+            <img src="../assets/img/dc-logo.png" alt="logo-dc" class="logo">
+        </div>
+        <div class="navbar-container">
+            <ul>
+                <li v-for="">
+
+                </li>
+            </ul>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
-    name: '',
+    
+    name: 'AppHeader',
     data(){
         return{
+            navbarElements : ["characters" , "comics" , "movies" , "tv" , "game" , "collectibles" , "videos" , "fans" , "news" , "shop"]
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.logo-container{
     
+    width: 35%;
+    text-align: center;
+    padding: 1rem;
+}
+
+.logo{
+    height: 80px;
+}
+
+header{
+    display: flex;
+}
 </style>
